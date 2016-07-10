1c025bc8af8391c03a85f62e2ae83dd1d56146d6
Frederic <fredericmbea@gmail.com>
2016-07-10 07:27:45 +0000

    fixed syntax errors in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :address => smtp.sendgrid.net,
+    :address => 'smtp.sendgrid.net',
     :port => '587',
-    :authentication => ':plain',
+    :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
