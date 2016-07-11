ca4614df81570fdcaf742489956ebdd12b5cee6d
Frederic <fredericmbea@gmail.com>
2016-07-11 18:54:33 +0000

    Added plan id to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711181702) do
+ActiveRecord::Schema.define(version: 20160711184105) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
